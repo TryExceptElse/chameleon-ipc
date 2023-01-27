d4186f3c7caa9ad86d89ddf5cd73b5f286630425
TryExceptElse <schwarzjohnv@gmail.com>
2023-01-26 19:37:03 -0800

    Add simple.h test resource.
    This file will be used to test the cipcc parser module,
    ensuring that simple IPC interfaces are parsed correctly.

---
+++ test-resources/simple.h
@@ -0,0 +1,45 @@
+/** @file
+ * Fake boilerplate...
+ */
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <string>
+
+namespace cipc::example {
+
+// @IPC(Serializable)
+enum class Mode {
+    Encabulate,
+    SideFumble,
+    Idle,
+};
+
+// @IPC(Callback)
+using ModeChangeCallback = std::function<Mode()>;
+
+// @IPC(Interface)
+class Component {
+  // @IPC(Method)
+  virtual std::string name() const;
+
+  // @IPC(Method)
+  virtual std::size_t id() const;
+
+  // @IPC(Method)
+  virtual Mode current_mode() const;
+
+  // @IPC(Method)
+  virtual void noop();
+
+  // @IPC(Method)
+  virtual int set_mode(Mode mode);
+};
+
+// @IPC(Interface)
+class System {
+  // @IPC(Method)
+  virtual std::vector<Component*> components() const;
+};
+
+}  // namespace cipcc::example
